9bd825158c3ab9a6183c9d33a5bcc274ca445801
Maness <C-Kenneth.Maness@charter.com>
2022-11-25 09:56:52 -0600

    create data package in backend for data tables

---
+++ backend/data/articles.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Article(Enum):
+    A = 1
+    AN = 2
+    THE = 3
---
+++ backend/data/verbs.py
@@ -0,0 +1,73 @@
+verbs = {
+    "examine": 1,
+    "look": 1,
+    "observe": 1,
+    "check": 1,
+    "explore": 1,
+    "probe": 1,
+    "view": 1,
+    "peruse": 1,
+    "scope": 1,
+    "investigate": 1,
+    "peer": 1,
+    "see": 1,
+    "stare": 1,
+    "watch": 1,
+    "glance": 1,
+    "behold": 1,
+    "ogle": 1,
+    "survey": 1,
+    "scan": 1,
+    "regard": 1,
+    "inspect": 1,
+    "gaze": 1,
+    "focus": 1,
+    "gawk": 1,
+
+    "take": 2,
+    
+    "drop": 3,
+
+    "open": 4,
+
+    "push": 5,
+
+    "pull": 6,
+
+    "feel": 7,
+    "touch": 7,
+
+    "eat": 8,
+
+    "drink": 9,
+
+    "fill": 10,
+
+    "smell": 11,
+
+    "sniff": 11,
+
+    "listen": 12,
+
+    "break": 13,
+
+    "burn": 14,
+
+    "climb": 15,
+
+    "wave": 16,
+
+    "wear": 17,
+
+    "sleep": 18,
+
+    "jump": 19,
+
+    "pray": 20,
+
+    "curse": 21,
+
+    "sing": 22,
+
+    "undo": 23
+}
